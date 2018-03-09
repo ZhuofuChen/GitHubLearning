71f5a8ea31f599341f973af5fdd8a967650d74c2
ZhuofuChen <chenzhuofusean@gmail.com>
2018-03-09 10:22:06 -0800

    Revert "fuck sid"

--- Test.java
+++ Test.java
@@ -1,6 +1,5 @@
 public class Test{
     public static void main(String[] args) {
         System.out.println("Hello, fuck Brother san");
-        System.out.println("Hello, fuck sid too");
     }
 }
